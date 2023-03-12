60214b170e217b1b941889832c12f8d3ca8b0a86
SaschaGrill <aasascha@hotmail.com>
2023-03-12 11:31:18 +0100

    function answer include

--- script.js
+++ script.js
@@ -7,28 +7,28 @@ let questions = [{
     "right_answer": 4,
 },
 {
-    "question": "blabla",
-    "answer_1": "blabla",
-    "answer_2": "blabla",
-    "answer_3": "blabla",
-    "answer_4": "blabla",
-    "right_answer": 3,
+    "question": "Was für ein Auto fahre ich?",
+    "answer_1": "Dacia",
+    "answer_2": "BMW",
+    "answer_3": "Audi",
+    "answer_4": "Porsche",
+    "right_answer": 1,
 },
 {
-    "question": "blabla",
-    "answer_1": "blabla",
-    "answer_2": "blabla",
-    "answer_3": "blabla",
-    "answer_4": "blabla",
-    "right_answer": 3,
+    "question": "Welcher ist der höchste Berg der Welt?",
+    "answer_1": "Pariser Pantheon",
+    "answer_2": "Matterhorn",
+    "answer_3": "Zugspitze",
+    "answer_4": "Mount Everest",
+    "right_answer": 4,
 },
 {
-    "question": "blabla",
-    "answer_1": "blabla",
-    "answer_2": "blabla",
-    "answer_3": "blabla",
-    "answer_4": "blabla",
-    "right_answer": 3,
+    "question": "Wofür stehen die olympischen Ringe?",
+    "answer_1": "Hoolahoop Reifen",
+    "answer_2": "Für die 5 Kontinente",
+    "answer_3": "Die 5 Schwierigkeitsgrade",
+    "answer_4": "Für die Automarke Audi",
+    "right_answer": 2,
 },
 {
     "question": "blabla",
@@ -49,6 +49,9 @@ let questions = [{
 ];
 
 let currentQuestion = 0;
+let questionnum = 1;
+let currentAnswer = 0;
+
 
 function init() {
     let maxlengthquestions = document.getElementById('max-length-questions');
@@ -58,11 +61,30 @@ function init() {
 
 function showQuestion() {
     let question = questions[currentQuestion];
-    let headquestion = document.getElementById('headquestion');
-    headquestion.innerHTML = question["question"]
-    for (let i = 1; i < 5; i++) {
-        let answer = question["answer_"+i];
-        let allanswer = document.getElementById(`answer${i}`)
-        allanswer.innerHTML = answer;
+    document.getElementById('headquestion').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+    questionOf();
+}
+
+function questionOf() {
+    let questionnumber = document.getElementById('questionnumber');
+    questionnumber.innerHTML = questionnum;
+    questionnum++;
+}
+
+function answer(answer) {
+    let question = questions[currentQuestion];
+    let questionnumber = answer.slice(-1);
+    
+    let idOfRightAnswer = `answer_${question["right_answer"]}`;
+
+    if(questionnumber == question['right_answer']) {
+        document.getElementById(answer).parentNode.classList.add('bg-success');
+    } else {
+        document.getElementById(answer).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
 }
